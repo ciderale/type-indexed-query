a3e58498a5f03921bc8ed2f6a93ba0af0ce8731e
Alain Lehmann <alain.lehmann@gmail.com>
2026-07-04 23:07:05 +0200

    add minimal test example

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     implementation("org.jooq:jooq:3.21.6")
     testImplementation(kotlin("test"))
+    testImplementation("org.postgresql:postgresql:42.7.3")
 }
 
 tasks.test {
---
+++ src/test/kotlin/io/github/ciderale/tik/JooqTest.kt
@@ -0,0 +1,48 @@
+package io.github.ciderale.tik
+
+import io.github.ciderale.tiq.core.Mode
+import io.github.ciderale.tiq.core.of
+import io.github.ciderale.tiq.sample.UserRepository
+import io.github.ciderale.tiq.sample.db.JooqUserRepository
+import org.jooq.DSLContext
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+import org.junit.jupiter.api.Test
+import java.sql.DriverManager
+
+class JooqTest {
+    private fun dsl(): DSLContext {
+        val conn =
+            DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/appdb",
+                "app",
+                "app",
+            )
+
+        return DSL.using(conn, SQLDialect.POSTGRES)
+    }
+
+    @Test
+    fun testSelectUsers() {
+        val ctx = dsl()
+        val repo = JooqUserRepository(ctx)
+
+        repo
+            .fetch(
+                UserRepository.Query(id = "1"),
+                Mode.One.of(UserRepository.Summary),
+            ).also(::println)
+
+        repo
+            .fetch(
+                UserRepository.Query(),
+                Mode.Many.of(UserRepository.Detail),
+            ).also(::println)
+
+        repo
+            .fetch(
+                UserRepository.Query(),
+                Mode.Paged(0, 2).of(UserRepository.Detail),
+            ).also(::println)
+    }
+}
