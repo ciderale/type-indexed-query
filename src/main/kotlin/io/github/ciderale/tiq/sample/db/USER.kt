2e34b1d25ab919ce31778be5cab758082144de37
Alain Lehmann <alain.lehmann@gmail.com>
2026-07-04 23:04:43 +0200

    fix jooq table metadata definition

--- src/main/kotlin/io/github/ciderale/tiq/sample/db/USER.kt
+++ src/main/kotlin/io/github/ciderale/tiq/sample/db/USER.kt
@@ -1,14 +1,23 @@
 package io.github.ciderale.tiq.sample.db
 
 import org.jooq.Record
+import org.jooq.TableField
 import org.jooq.impl.DSL
+import org.jooq.impl.SQLDataType
 import org.jooq.impl.TableImpl
 
 object USER : TableImpl<Record>(DSL.name("user")) {
-    val ID = field(DSL.name("id"), String::class.java)
-    val NAME = field(DSL.name("name"), String::class.java)
-    val EMAIL = field(DSL.name("email"), String::class.java)
-    val ACTIVE = field(DSL.name("active"), String::class.java)
+    val ID: TableField<Record, String> =
+        createField(DSL.name("id"), SQLDataType.VARCHAR)
+
+    val NAME: TableField<Record, String> =
+        createField(DSL.name("name"), SQLDataType.VARCHAR)
+
+    val EMAIL: TableField<Record, String> =
+        createField(DSL.name("email"), SQLDataType.VARCHAR)
+
+    val ACTIVE: TableField<Record, Boolean> =
+        createField(DSL.name("active"), SQLDataType.BOOLEAN)
 
     override fun getSchema() = null
 
